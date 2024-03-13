0325375cc0a1cf2a0b9ceae1b6727ffa50ad60dc
NevermoreBetter <72225613+NevermoreBetter@users.noreply.github.com>
2024-03-13 15:32:17 +0200

    'finish'

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,26 +1,41 @@
 "use client";
-import "@blocknote/core/style.css";
-import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
-import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
-import { useTheme } from "next-themes";
 
-interface IEditor {
+import { useTheme } from "next-themes";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+
+import { useEdgeStore } from "@/lib/edgestore";
+
+interface EditorProps {
  onChange: (value: string) => void;
  initialContent?: string;
  editable?: boolean;
 }
 
-const Editor = ({ onChange, initialContent, editable }: IEditor) => {
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
  const { resolvedTheme } = useTheme();
- const editor: BlockNoteEditor = useCreateBlockNote({
+ const { edgestore } = useEdgeStore();
+
+ const handleUpload = async (file: File) => {
+  const response = await edgestore.publicFiles.upload({
+   file,
+  });
+
+  return response.url;
+ };
+
+ const editor: BlockNoteEditor = useBlockNote({
   editable,
   initialContent: initialContent
    ? (JSON.parse(initialContent) as PartialBlock[])
    : undefined,
-  useEditorChange: (editor) => {
+  onEditorContentChange: (editor) => {
    onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
   },
+  uploadFile: handleUpload,
  });
+
  return (
   <div>
    <BlockNoteView
